8472798fd489683b971e16048b0e8b29be0d6a48
zhanjq <1187625098@qq.com>
2021-10-19 21:39:02 +0800

    项目初始化

---
+++ src/main/java/com/zjq/concurrency/ConcurrencyApplication.java
@@ -0,0 +1,17 @@
+package com.zjq.concurrency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 并发
+ * @author zjq
+ */
+@SpringBootApplication
+public class ConcurrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrencyApplication.class, args);
+    }
+
+}
